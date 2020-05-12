1657c8372ccd239b299f53cf203f3b4963cb5aca
ripanSh <47637681+ripanSh@users.noreply.github.com>
2020-05-12 19:53:38 +0530

    Update videoScript.js

--- videoScript.js
+++ videoScript.js
@@ -3,7 +3,7 @@
  }
 
  var newEl = document.createElement('div');
- newEl.innerHTML = "<a id="TTE-f13de5f4-2e6d-4b22-8bcb-26b79350466c" href="https://10to8.com/book/qwbdseokgmtjahpbtm/" target="_blank">See Online Booking Page</a> <script src="https://d3saea0ftg7bjt.cloudfront.net/embed/js/embed.min.js"></script> <script> window.TTE.init({ targetDivId: "TTE-f13de5f4-2e6d-4b22-8bcb-26b79350466c", uuid: "f13de5f4-2e6d-4b22-8bcb-26b79350466c", }); </script>";
+ newEl.innerHTML = '<a id="TTE-f13de5f4-2e6d-4b22-8bcb-26b79350466c" href="https://10to8.com/book/qwbdseokgmtjahpbtm/" target="_blank">See Online Booking Page</a> <script src="https://d3saea0ftg7bjt.cloudfront.net/embed/js/embed.min.js"></script> <script> window.TTE.init({ targetDivId: "TTE-f13de5f4-2e6d-4b22-8bcb-26b79350466c", uuid: "f13de5f4-2e6d-4b22-8bcb-26b79350466c", }); </script>';
 
 var element = document.getElementById("shopify-section-product-template");
 
