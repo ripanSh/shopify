db9db273688d9f5c2da28f75a3bf883cdfb0c354
ripanSh <47637681+ripanSh@users.noreply.github.com>
2020-05-12 19:24:10 +0530

    Update videoScript.js

--- videoScript.js
+++ videoScript.js
@@ -1,1 +1,8 @@
-console.log("Alert");
+var element = document.getElementById("shopify-section-product-template");
+ 
+//If it isn't "undefined" and it isn't "null", then it exists.
+if(typeof(element) != 'undefined' && element != null){
+    alert('Element exists!');
+} else{
+    alert('Element does not exist!');
+}
